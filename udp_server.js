fb1c220d2cf1f06711c53a98ffefb41d5116703f
Mike O'Dell <mike.o'dell@pgi.com>
2014-07-24 22:33:00 -0400

    Initial Commit

---
+++ udp_server.js
@@ -0,0 +1,18 @@
+var PORT = 33333;
+var HOST = '127.0.0.1'
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function() {
+  var address = server.address;
+  console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function(message, remote) {
+  console.log(remote.address + ":" + remote.port + ' - ' + message);
+  var message = new Buffer("OK"); 
+  server.send(message, 0, message.length, remote.port, remote.address);
+});
+
+server.bind(PORT, HOST);
