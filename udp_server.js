d1a3b6ba3a59d49d9d31326fbcd2944c89855763
Mike O'Dell <mike.o'dell@pgi.com>
2014-08-18 21:37:41 -0400

    update to node script

--- udp_server.js
+++ udp_server.js
@@ -1,5 +1,7 @@
 var PORT = 33333;
 var HOST = '172.31.38.37';
+var request = require('request');
+var FormData = require('form-data');
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
@@ -10,13 +12,23 @@ server.on('listening', function() {
 });
 
 server.on('error', function(err) {
-  console.log(err);
+  console.log('error : ' + err);
 });
 
 server.on('message', function(message, remote) {
   console.log(remote.address + ":" + remote.port + ' - ' + message);
-  var message = new Buffer("ok");
-  server.send(message, 0, message.length, remote.port, remote.address);
+
+  var form = new FormData();
+  form.append("From", remote.address);
+  form.append("Body", message);
+
+  form.submit('http://envicor-stats.herokuapp.com/new_board_event', function(err, res) {
+    res.resume();
+  });
+
+  var server_response = new Buffer("ok");
+  server.send(server_response, 0, server_response.length, remote.port, remote.address);
+
 });
 
 server.bind(PORT, HOST);
